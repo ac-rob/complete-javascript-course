21dbabf13afababa43b7417c84cc2c1fe9889ec3
Unknown <a_roberts252@hotmail.co.uk>
2019-03-11 11:42:34 +0000

    variable mutation and coercion

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,43 @@
+/********************
+Variables and data types
+*/
+/*
+var firstName = 'Anna';
+
+var lastName ='Roberts';
+
+var age = '26';
+
+var job = true;
+
+console.log(firstName,lastName,age,job);
+*/
+/********************
+Variable mutation and type coercion
+
+
+var firstName ='Anna';
+var age = '26';
+Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is she married? ' + isMarried + '.');
+
+Variable mutation
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is she married? ' + isMarried + '.');
+
+var lastName = prompt('What is his last name?');
+console.log(firstName, lastName);
+*/
+//Requesting album reviews
+
+var artistName = prompt('Who is the artist?');
+var albumName = prompt('What is the album called?');
+var albumRating = prompt('What is the rating?');
+
+alert('You rated ' + albumName + ' by ' + artistName + albumRating + ' stars.');
+console.log('You rated ' + albumName + ' by ' + artistName + ' ' + albumRating + ' stars.');
